014f1a57dfaa4ed5340c13ffd15996174fa82fef
Mark van Kleef <mkleef@avans.nl>
2018-01-03 14:04:26 +0100

    Added mr and mrs janssen

--- src/main.cpp
+++ src/main.cpp
@@ -22,9 +22,9 @@ int main() {
         kmint::base_systems base {};
         kmint::board s {};
         kmint::background b { kmint::image { "resources/Graph.png" } };
-        kmint::graph_bound_board_piece rabbit { g, 1000, kmint::image { "resources/bunnelby.png", 0.33f } };
-        kmint::graph_bound_board_piece meneer { g, 1000, kmint::image { "resources/meneerjanssen.png", 0.33f } };
-        kmint::graph_bound_board_piece mevrouw { g, 1000, kmint::image { "resources/mevrouwjanssen.png", 0.33f } };
+        kmint::Bunny rabbit { kmint::point { 380, 360}, kmint::image { "resources/bunnelby.png", 0.33f } };
+        kmint::graph_bound_board_piece meneer { g, 5, kmint::image { "resources/meneerjanssen.png", 0.33f } };
+        kmint::graph_bound_board_piece mevrouw { g, 1570, kmint::image { "resources/mevrouwjanssen.png", 0.33f } };
         kmint::Cow sheep{ g, 0, kmint::image { "resources/poochyena.png", 0.25f }};
         //kmint::Bunny t{ kmint::point{10,10},kmint::image { "resources/cow.png", 0.25f }};
         kmint::a_star search;
@@ -36,6 +36,8 @@ int main() {
         search.search(&g,start, end);
         s.add_board_piece(b);
         s.add_board_piece(g);
+        s.add_board_piece(meneer);
+        s.add_board_piece(mevrouw);
 
         s.add_board_piece(sheep);
         //s.add_board_piece(t);
