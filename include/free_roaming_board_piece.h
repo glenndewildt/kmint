efed64977e335106881ab1dd34e7cf1088f7f1ce
Unknown <gwildt@avans.nl>
2018-01-03 14:56:46 +0100

    Auto stash before merge of "master" and "origin/master"

--- include/free_roaming_board_piece.h
+++ include/free_roaming_board_piece.h
@@ -9,6 +9,7 @@ namespace kmint {
         point _location;
     public:
         free_roaming_board_piece(point location) : board_piece {}, _location {location} {}
+        void set_point(point pos){ _location = pos;}
         point location() const override { return _location; }
     };
 }
