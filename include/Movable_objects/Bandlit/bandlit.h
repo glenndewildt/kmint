ec5250c1cb333e1633ae4156a6156d012338ddc5
Unknown <gwildt@avans.nl>
2018-04-07 20:08:04 +0200

    Added bandlid identifiers

--- include/Movable_objects/Bandlit/bandlit.h
+++ include/Movable_objects/Bandlit/bandlit.h
@@ -10,17 +10,23 @@
 
 namespace kmint {
     class bandlit: public state_object {
+
+
+
     private:
         city* stad;
 
     public:
+        enum Name { AXEL, ANDRE, FRANS, JOHNNIE };
+
+        Name name;
         int money;
         double pause;
         double time;
         double wait;
 
 
-        bandlit(const graph &graph, node_id nid, const image &i, city* city):stad{city},pause{0},time{0}, wait{0},money{1000},state_object(graph,nid,i){
+        bandlit(const graph &graph, node_id nid, const image &i, city* city, Name name ):name{name},stad{city},pause{0},time{0}, wait{0},money{1000},state_object(graph,nid,i){
         };
 
         virtual void update(float dt, std::vector< board_piece*> &_board_pieces)override {
